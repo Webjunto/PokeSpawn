b25680d7f892c80a3b89be78c6c084bbfc594694
Jedi Weller <jedidiahweller@gmail.com>
2016-07-13 20:03:23 -0400

    Redoing Home Page & providing console logs
    channel.html —>  Updating Copyright and title
    main.controller.js —>  Fixing indentation
    main.html —>  Removing Tree Chart & updating caption

--- client/app/controllers/main/main.controller.js
+++ client/app/controllers/main/main.controller.js
@@ -1,10 +1,12 @@
 'use strict';
 
 angular.module('tophemanDatavizApp')
-        .controller('MainCtrl', function($scope, persistance, displayState) {
 
-          $scope.channelsDescription = persistance.getData().channelsDescription;
-          $scope.data = persistance.getData();
-          $scope.displayState = displayState;
+.controller('MainCtrl', function($scope, persistance, displayState) {
 
-        });
+  $scope.channelsDescription = persistance.getData().channelsDescription;
+  $scope.data = persistance.getData();
+  $scope.displayState = displayState;
+
+  // console.log("Data = " + JSON.stringify($scope.data));
+});
