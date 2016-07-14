b1d8e7599a2d4d865824d700047dfe70bf80fb13
Jedi Weller <jedidiahweller@gmail.com>
2016-07-14 19:21:52 -0400

    Updating main controller to display icons for *every* post we have received

--- client/app/controllers/main/main.controller.js
+++ client/app/controllers/main/main.controller.js
@@ -2,14 +2,14 @@
 
 angular.module('tophemanDatavizApp')
 
-.controller('MainCtrl', function($scope, persistance, displayState) {
+.controller('MainCtrl', function($scope, $http, persistance, displayState) {
 
   $scope.channelsDescription = persistance.getData().channelsDescription;
   $scope.data = persistance.getData();
   $scope.displayState = displayState;
 
   $scope.$watchCollection('data', function(newData, oldData) {
-    // console.log("Data has changed!, data: " + JSON.stringify(newData.channels[2]));
+    console.log("Data has changed!, data: " + JSON.stringify(newData.channels[2]));
     if (newData.channels[2].lastTweets[0]) {
       var lat = newData.channels[2].lastTweets[0].coordinates[0];
       var lon = newData.channels[2].lastTweets[0].coordinates[1];
@@ -21,14 +21,13 @@ angular.module('tophemanDatavizApp')
         position: tmpCoordinates
       });
     }
-    
   });
 
   var newyork = new google.maps.LatLng(40.69847032728747, -73.9514422416687);
 
   var mapOptions = {
     center: newyork,
-    zoom: 4,
+    zoom: 1,
     mapTypeId: google.maps.MapTypeId.ROADMAP
   };
 
@@ -49,21 +48,33 @@ angular.module('tophemanDatavizApp')
   //Wait until the map is loaded
   google.maps.event.addListenerOnce($scope.map, 'idle', function(){
     console.log("Google Maps Idle");
-    var marker = new google.maps.Marker({
-        map: $scope.map,
-        animation: google.maps.Animation.DROP,
-        position: newyork
-    });
 
-    var infoWindow = new google.maps.InfoWindow({
-        content: "Here I am!"
-    });
-   
-    google.maps.event.addListener(marker, 'click', function () {
-        infoWindow.open($scope.map, marker);
-    });      
-   
-  });
+    var getURL = window.location + "tweets";
+    console.log("GET URL = " + getURL);
+    $http.get(getURL) .then(function(response){ 
+      
+      for (var r in response.data) {
+        // console.log("*******" + JSON.stringify(response.data[r]));
+
+        var lat = response.data[r].coordinates[0];
+        var lon = response.data[r].coordinates[1];
+        var tmpCoordinates = new google.maps.LatLng(lat, lon);
+
+        var marker = new google.maps.Marker({
+          map: $scope.map,
+          animation: google.maps.Animation.DROP,
+          position: tmpCoordinates
+        });
+
+        // var infoWindow = new google.maps.InfoWindow({
+        //   content: response.data[r].keywords[0]
+        // });
   
-    // end code to get map running //
+        // google.maps.event.addListener(marker, 'click', function () {
+        //     infoWindow.open($scope.map, marker);
+        // }); 
+      }
+    });
+  });
+  // end code to get map running //
 });
