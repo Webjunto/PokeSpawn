05313d956b45c9adf0eb82628f088cf08b1fca95
Jedi Weller <jedidiahweller@gmail.com>
2016-07-15 12:58:40 -0400

    Server side fixes for production mode
    Google maps must be HTTPS
    Adding Mongoose connection string

--- server/config/express.js
+++ server/config/express.js
@@ -29,10 +29,14 @@ module.exports = function(app) {
   app.use(cookieParser());
 
   if ('production' === env) {
+    console.log("****Production was set****");
     app.use(favicon(path.join(config.root, 'public', 'favicon.ico')));
     app.use(express.static(path.join(config.root, 'public')));
     app.set('appPath', config.root + '/public');
     app.use(morgan('dev'));
+
+    mongoose.connect("mongodb://heroku_x4rm9c3c:3j0t2u324t533jg3gf2moo9q74@ds021979.mlab.com:21979/heroku_x4rm9c3c");
+
   }
 
   if ('development' === env || 'development-online' === env || 'test' === env) {
