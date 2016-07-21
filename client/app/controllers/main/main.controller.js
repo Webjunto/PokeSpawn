5549e71d19eecdfa6826c7573f898785714afbc6
Jedi Weller <jedidiahweller@gmail.com>
2016-07-21 08:48:48 -0400

    Refactor main.controller so that onZoom changes the query and results

--- client/app/controllers/main/main.controller.js
+++ client/app/controllers/main/main.controller.js
@@ -8,8 +8,9 @@ angular.module('tophemanDatavizApp')
   $scope.data = persistance.getData();
   $scope.displayState = displayState;
   $scope.queryCount;
-  //Initialize Map 
-  $scope.map = gservice.createMap();
+
+  $scope.formData = {};
+  var queryBody = {};
 
   $scope.$watchCollection('data', function(newData, oldData) {
     console.log("Data has changed!, data: " + JSON.stringify(newData.channels[0]));
@@ -18,44 +19,35 @@ angular.module('tophemanDatavizApp')
     }
   });
 
-  // begin code to drop a marker & create info window (this MUST be inside where the map loads: http://stackoverflow.com/questions/7058094/google-maps-api-cannot-read-property-e3-of-undefined) //
-  //Wait until the map is loaded
-  google.maps.event.addListenerOnce($scope.map, 'idle', function(){
-    console.log("Google Maps Idle");
-    var getTweets = window.location + "tweets";
-
-    $http.get(getTweets).then(function(response){ 
-      $scope.queryCount = response.data.length;
-      for (var r in response.data) {
-        if (r >= 750) { 
-          return;
-        }
-        var promise = new Promise(function(resolve, reject) {
-          // do a thing, possibly async, then…
-          if (gservice.createMarker(response.data[r])) {
-            resolve("Creating marker worked");
-          }
-          else {
-            reject(Error("Creating marker didn't work!"));
-          }
-        });
-      }
+  var addEventListeners = function () {
+    console.log("Adding map event listeners");
+    // begin code to drop a marker & create info window (this MUST be inside where the map loads: http://stackoverflow.com/questions/7058094/google-maps-api-cannot-read-property-e3-of-undefined) //
+    //Wait until the map is loaded
+   google.maps.event.addListenerOnce($scope.map, 'idle', function(){
+      $scope.queryTweets();
     });
-  });
 
-  /* Query Controlls */
-
-  // Initializes Variables
-  // ----------------------------------------------------------------------------
-  $scope.formData = {};
-  var queryBody = {};
-
+    google.maps.event.addListener($scope.map, 'zoom_changed', function() {
+      console.log("zoom changed!");
+      // gservice.reloadMarkers();
+      $scope.queryTweets();
+    });
+  }
+  //Initialize Map 
+  $scope.map = gservice.createMap();
+  addEventListeners();
+  
   // Take query parameters and incorporate into a JSON queryBody
-  $scope.queryTweets = function(){
+  $scope.queryTweets = function(zoomChange){
+    console.log("Quering for Tweets");
     /* Get our current map state so we can return here upon successful query */
     var tmpCenter = $scope.map.getCenter();
     var tmpZoom = $scope.map.getZoom();
-    console.log("MAP CENTER: " + JSON.stringify(tmpCenter) + " and Zoom = " + tmpZoom);
+    console.log("Map Center:" + JSON.stringify(tmpCenter) + ", Zoom = " + tmpZoom + " , Distance = " + $scope.formData.distance);
+
+    if (!$scope.formData.distance) {
+      $scope.formData.distance = 1000;
+    }
     
     // Assemble Query Body
     queryBody = {
@@ -64,7 +56,7 @@ angular.module('tophemanDatavizApp')
       maxAge: $scope.formData.maxAge,
       latitude: parseFloat(tmpCenter.lat()),
       longitude: parseFloat(tmpCenter.lng()),
-      distance: parseFloat($scope.formData.distance) //100 miles default
+      distance: parseFloat($scope.formData.distance)
     };
 
     if (queryBody.pokemon) {
@@ -74,41 +66,25 @@ angular.module('tophemanDatavizApp')
     console.log("Query = " + JSON.stringify(queryBody));
     // Post the queryBody to the /query POST route to retrieve the filtered results
     $http.post('/query', queryBody)
-
-      // Store the filtered results in queryResults
       .success(function(queryResults)
       {
-        // Query Body and Result Logging
-        console.log("QueryBody:");
-        console.log(queryBody);
         // Count the number of records retrieved for the panel-footer
         $scope.queryCount = queryResults.length;
-        console.log("Query count = " + $scope.queryCount);
-  
-        $scope.map = gservice.createMap(tmpCenter, tmpZoom);
-        /* End Reset the Map*/ 
+        console.log("Query results retrieved: " + $scope.queryCount);
 
-        google.maps.event.addListenerOnce($scope.map, 'idle', function(){
-          for (var r in queryResults) {
-            var promise = new Promise(function(resolve, reject) {
-              // console.log("...");
-              // console.log(JSON.stringify(queryResults[r]));
-              // console.log("...");
-              if (gservice.createMarker(queryResults[r])) {
-                resolve("Creating marker worked");
-              }
-              else {
-                reject(Error("Creating marker didn't work!"));
-              }
-            });
+        for (var r in queryResults) {
+        var promise = new Promise(function(resolve, reject) {
+          if (gservice.createMarker(queryResults[r])) {
+            resolve("Creating marker worked");
+          }
+          else {
+            reject(Error("Creating marker didn't work!"));
           }
         });
+        }
       })
       .error(function(queryResults){
           console.error('Error in Query' + queryResults);
       })
     };
-
-
-    /* End Query Controls */
 });
